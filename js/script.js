d06f3b5568df78c9da013e6effa952b92339b0ed
wallsmanBR <alves_thiagobastos@hotmail.com>
2018-06-02 15:37:26 -0300

    tela inicial

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Get the modal
-var modal = document.getElementById('id01');
-var retorno = document.getElementById('modal-content');
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
